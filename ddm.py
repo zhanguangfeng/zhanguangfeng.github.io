3544b0431181891d6e0fc4e084676aaa30121be7
Frank <zhan@mac.local>
2023-12-05 10:33:21 +0800

    20231205

---
+++ ddm.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+import excel
+import json
+
+wb = excel.open('ddm.xls')
+sh = wb[0]
+js = None
+with open('ddm.js') as fp:
+    tmp, jss = fp.read().split('=')
+    js = json.loads(jss)
+
+d = {}
+a = []
+for r in sh.rows:
+    l = r[3].value
+    s = r[7].value
+    d[l] = s
+    v = js[l]
+    if v != s:
+        a.append( '{:24}: \033[31m{}\033[0m -> \033[32m{}\033[0m'.format(l, v, s) )
+    else:
+        print( '{:24}: {}'.format(l, s) )
+
+if a: print('-' * 31)
+for s in a:
+    print(s)
+j = 'var ddm = '
+s = json.dumps(d, sort_keys=True, indent=4)
+f = open('ddm.js', 'w')
+f.write(j + s)
